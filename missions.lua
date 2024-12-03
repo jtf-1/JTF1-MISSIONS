489e536d865a7aa25c098b8ba44d587b8062e769
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2024-12-03 17:44:34 +0000

    server move

---
+++ missions.lua
@@ -0,0 +1,38 @@
+MISSIONLIST = {
+    {
+        missionName = "Caucasus Fun Map",
+        missionMenu = {
+            {menuText = "Day", missionFile = "JTF-1_Caucasus_DAY_Fun_Map.miz"},
+            {menuText = "Day Weather", missionFile = "JTF-1_Caucasus_DAY_WEATHER_Fun_Map.miz"},
+            {menuText = "Night", missionFile = "JTF-1_Caucasus_NIGHT_Fun_Map.miz"},
+            {menuText = "Night Weather", missionFile = "JTF-1_Caucasus_NIGHT_WEATHER_Fun_Map.miz"},
+        },
+    },
+    {
+        missionName = "PG2 Fun Map",
+        missionMenu = {
+            {menuText = "Day", missionFile = "JTF-1_PG2_DAY_Fun_Map.miz"},
+            {menuText = "Day Weather", missionFile = "JTF-1_PG2_DAY_WEATHER_Fun_Map.miz"},
+            {menuText = "Night", missionFile = "JTF-1_PG2_NIGHT_Fun_Map.miz"},
+            {menuText = "Night Weather", missionFile = "JTF-1_PG2_NIGHT_WEATHER_Fun_Map.miz"},
+        },
+    },
+    {
+        missionName = "NTTR Fun Map",
+        missionMenu = {
+            {menuText = "Day", missionFile = "JTF-1_NTTR_DAY_Fun_Map.miz"},
+            {menuText = "Day Weather", missionFile = "JTF-1_NTTR_DAY_WEATHER_Fun_Map.miz"},
+            {menuText = "Night", missionFile = "JTF-1_NTTR_NIGHT_Fun_Map.miz"},
+            {menuText = "Night Weather", missionFile = "JTF-1_NTTR_NIGHT_WEATHER_Fun_Map.miz"},
+        },
+    },
+    {
+        missionName = "Afghanistan Fun Map",
+        missionMenu = {
+            {menuText = "Day", missionFile = "JTF-1_AFG_DAY_Fun_Map.miz"},
+            {menuText = "Day Weather", missionFile = "JTF-1_AFG_DAY_WEATHER_Fun_Map.miz"},
+            {menuText = "Night", missionFile = "JTF-1_AFG_NIGHT_Fun_Map.miz"},
+            {menuText = "Night Weather", missionFile = "JTF-1_AFG_NIGHT_WEATHER_Fun_Map.miz"},
+        },
+    },
+}
